a2b215fbed9af5f4a3614ecd913f452ebfff5804
Leandro Motta M. Oliveira <leandrommoliveira@gmail.com>
2021-11-15 21:03:35 -0300

    initial commit

---
+++ src/clojure_web/core.clj
@@ -0,0 +1,23 @@
+(ns clojure-web.core 
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.params :refer [wrap-params]]))
+
+(defn myapp [request]
+  (str "Hello, " (get (:params request) "name")))
+
+(defn string-response-middleware [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (if (instance? String response)
+        {:body response
+         :status 200
+         :headers {"Content-Type" "text/html"}}
+        response))))
+
+(def handler
+  (-> myapp
+      string-response-middleware
+      wrap-params))
+
+(defn -main []
+  (jetty/run-jetty handler {:port 3000}))
